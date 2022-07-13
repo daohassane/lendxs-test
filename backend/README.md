80d595ead475889676f400122964a0c14313c652
Hassane DAO <dao.hassane@gmail.com>
2022-07-13 17:51:12 +0000

    feat(): setup back project

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# launch server
+to launch the backend server make sure you have Docker installed on your machine 
+then do a build and run the following command:
+```cmd
+docker-compose up
+```
---
+++ backend/src/test/java/com/lendxs/acme/AcmeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lendxs.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
